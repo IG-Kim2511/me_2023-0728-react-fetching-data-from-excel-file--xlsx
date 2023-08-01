5178ceb26b0dd56c343bd2e4f4f3d968c0b861d8
IG <iikim2511@gmail.com>
2023-08-01 14:39:52 -0400

    clear

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import ExcelDataFetcher from './test';
+
+
+import ExcelDataFetcher from './components/ExcelDataFetcher';
 
 function App() {
   return (
     <div className="App">
  
 
-      <App2/>
       <ExcelDataFetcher/>
     </div>
   );
 }
 
-
-const App2 = () => {
-  return (
-    <div>
-    
-    <h1>
-    hi
-    </h1>
-    
-    
-    
-    </div>
-  )
-}
 
 
 export default App;
