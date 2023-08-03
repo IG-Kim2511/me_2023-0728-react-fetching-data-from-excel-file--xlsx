bca4cb015d636dafac3a005d3a0b47343dec1f00
IG <iikim2511@gmail.com>
2023-08-03 12:13:02 -0400

    tab Page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@
 import ExcelDataFetcher from './components/ExcelDataFetcher';
 import JoinUp from './components/JoinUp';
 import JsonDataFetching from './components/JsonDataFetching';
+import TabPage from './components/TabPage/TabPage';
 
 function App() {
   return (
     <div className="App">
  
 
-    <JoinUp/>
+
+    <TabPage/>
+
+      <JoinUp/>
+
       <ExcelDataFetcher/>
 
       <JsonDataFetching/>
---
+++ src/components/TabPage/TabPage.js
@@ -0,0 +1,42 @@
+// TabPage.js
+import React, { useState } from 'react';
+import "./TabPage.css";
+
+const TabPage = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTabIndex(index);
+  };
+
+  return (
+    <div className="tab-page">
+      <div className="tabs">
+        <Tab label="Tab 1" index={0} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+        <Tab label="Tab 2" index={1} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+        <Tab label="Tab 3" index={2} activeTabIndex={activeTabIndex} onClick={handleTabClick} />
+      </div>
+      <div className="tab-content">
+        {activeTabIndex === 0 && <p>Content of Tab 1</p>}
+        {activeTabIndex === 1 && <p>Content of Tab 2</p>}
+        {activeTabIndex === 2 && <p>Content of Tab 3</p>}
+      </div>
+    </div>
+  );
+};
+
+const Tab = ({ label, index, activeTabIndex, onClick }) => {
+    const isActive = index === activeTabIndex;
+  
+    return (
+      <div
+        className={`tab ${isActive ? 'active' : ''}`}
+        onClick={() => onClick(index)}
+      >
+        {label}
+      </div>
+    );
+  };
+
+
+export default TabPage;
